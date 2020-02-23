b48a49a9ae638b6967e962f6906c9c41040b99dc
Lorenzo Lando <lorenzo.lando314@gmail.com>
2020-02-23 23:11:18 +0000

    vue js test cli

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+   <ninja></ninja>
+  </div>
+</template>
+
+<script>
+export default {
+ data () {
+    return {
+      title: 'Ninja app'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuejs-playlist/src/Ninja.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="ninja in Ninjas">
+            {{ninja}}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+ data () {
+    return {
+      Ninjas: ['Lorenzo', 'Paolo', 'Marco']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
